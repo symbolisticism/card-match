5cc96e74cf3354f4821c319744c589a4f96c1a20
Gabriele Peck <peck.gabriele@gmail.com>
2023-06-28 15:47:59 -0700

    added a data folder to hold a list of cards and images for both sides of the cards

---
+++ lib/data/cards_list.dart
@@ -0,0 +1,8 @@
+import 'package:card_match/models/card.dart';
+import 'package:flutter/material.dart';
+
+final front = Image.asset('assets/images/fleur_de_lis.png');
+
+// const cardsList = [
+  
+// ]
