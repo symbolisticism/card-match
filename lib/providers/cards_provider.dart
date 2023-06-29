992efe58f126c838bc14a2c98ea211c34d9ec51b
Gabriele Peck <peck.gabriele@gmail.com>
2023-06-29 12:57:22 -0700

    added a providers folder and started some providers to help manage state

---
+++ lib/providers/cards_provider.dart
@@ -0,0 +1,4 @@
+import 'package:card_match/data/cards_list.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final cardsProvider = Provider((ref) => cardsList);
---
+++ lib/providers/game_loop_provider.dart
@@ -0,0 +1,10 @@
+import 'package:card_match/widgets/card_grid_item.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class GameLoopNotifier extends StateNotifier<List<CardGridItem>> {
+  GameLoopNotifier() : super([]);
+}
+
+final gameLoopProvider =
+    StateNotifierProvider<GameLoopNotifier, List<CardGridItem>>(
+        (ref) => GameLoopNotifier());
