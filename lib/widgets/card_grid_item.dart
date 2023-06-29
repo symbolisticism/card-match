a7e893496c6a9d711364607d249de7243f73a984
Gabriele Peck <peck.gabriele@gmail.com>
2023-06-28 18:10:36 -0700

    added variables to hold front and back images on the cards

--- lib/widgets/card_grid_item.dart
+++ lib/widgets/card_grid_item.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class CardGridItem extends StatelessWidget {
-  const CardGridItem({super.key});
+class CardGridItem extends StatefulWidget {
+  const CardGridItem({
+    super.key,
+    required this.front,
+    required this.back,
+  });
+
+  final Image front;
+  final Image back;
 
   @override
+  State<CardGridItem> createState() => _CardGridItemState();
+}
+
+class _CardGridItemState extends State<CardGridItem> {
+  bool hidden = true;
+  
+  @override
   Widget build(BuildContext context) {
+
     return GestureDetector(
+      onTap: () {
+        setState(() {
+          hidden = !hidden;
+          print(hidden);
+        });
+      },
       child: Container(
+        margin: const EdgeInsets.all(8),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(image: Image.asset('assets/images/fleur_de_lis.png').image),
+          image: DecorationImage(
+              image: hidden ? widget.front.image : widget.back.image),
         ),
       ),
     );
