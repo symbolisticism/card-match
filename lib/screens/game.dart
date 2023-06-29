03dafb97c7bb11cd1ead4270f4c45a2049163382
Gabriele Peck <peck.gabriele@gmail.com>
2023-06-29 00:14:07 -0700

    added _loadList function, in process of tracking card index/identifier to be able turn specific cards over instead of all or none

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -2,8 +2,51 @@ import 'package:card_match/data/cards_list.dart';
 import 'package:flutter/material.dart';
 import 'package:card_match/widgets/card_grid_item.dart';
 
-class Game extends StatelessWidget {
+class Game extends StatefulWidget {
   const Game({super.key});
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  late bool _isHidden;
+  late int _counter;
+  late List<CardGridItem> _matchingCards = [];
+
+
+  void _toggleVisibility() {
+    if (_counter != 2) {
+      // _isHidden = !_isHidden;
+      _counter++;
+    } else {
+      setState(() {
+        _isHidden = !_isHidden;
+      });
+      _counter = 1;
+    }
+  }
+
+  void _loadList() {
+    for (final card in cardsList) {
+      _matchingCards.add(
+      CardGridItem(
+          front: card.front,
+          back: card.back,
+          matchingId: card.matchingId,
+          hidden: _isHidden,
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _isHidden = true;
+    _counter = 1;
+    _loadList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +63,7 @@ class Game extends StatelessWidget {
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
         ),
-        children: [
-          for (final card in cardsList)
-            CardGridItem(front: card.front, back: card.back)
-        ],
+        children: _matchingCards,
       ),
     );
   }
