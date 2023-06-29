d4aef288ebb4832b8599fc4dc3bc4633a17bb4c1
Gabriele Peck <peck.gabriele@gmail.com>
2023-06-28 17:01:20 -0700

    finished the list of cards for the gridview

--- lib/data/cards_list.dart
+++ lib/data/cards_list.dart
@@ -4,5 +4,36 @@ import 'package:flutter/material.dart';
 final front = Image.asset('assets/images/fleur_de_lis.png');
 
 final cardsList = [
-  MatchCard(front: front, back: front),
-]
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/curvy_lines.png'),
+  ),
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/flowers.png'),
+  ),
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/glass_house.png'),
+  ),
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/money.png'),
+  ),
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/rooster.png'),
+  ),
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/spanish_mission.png'),
+  ),
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/water.png'),
+  ),
+  MatchCard(
+    front: front,
+    back: Image.asset('assets/images/books.png'),
+  ),
+];
