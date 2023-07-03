7ecbf387207662c4680066215c31d3f07709bb85
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-03 14:55:17 -0700

    made major changes, removed loadList function, added a provider

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,40 +1,15 @@
-import 'package:card_match/data/cards_list.dart';
+import 'package:card_match/providers/cards_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:card_match/widgets/card_grid_item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class Game extends ConsumerStatefulWidget {
-  const Game({
-    super.key,
-    required this.matchingCards,
-  });
-
-  final List<CardGridItem> matchingCards;
+  const Game({super.key});
 
   @override
   ConsumerState<Game> createState() => _GameState();
 }
 
 class _GameState extends ConsumerState<Game> {
-  void _loadList() {
-    for (final card in cardsList) {
-      widget.matchingCards.add(
-        CardGridItem(
-          front: card.front,
-          back: card.back,
-          matchingId: card.matchingId,
-          hidden: true,
-        ),
-      );
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadList();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +25,7 @@ class _GameState extends ConsumerState<Game> {
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
         ),
-        children: widget.matchingCards,
+        children: ref.watch(cardsProvider),
       ),
     );
   }
