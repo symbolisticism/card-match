f260146dea4329923a3c07d06f19856e21f934c5
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-03 14:50:32 -0700

    added index to the list, and I made it a list of CardGridItems rather than MatchCards (from the data model)

--- lib/data/cards_list.dart
+++ lib/data/cards_list.dart
@@ -1,47 +1,63 @@
-import 'package:card_match/models/match_card.dart';
+import 'package:card_match/widgets/card_grid_item.dart';
 import 'package:flutter/material.dart';
 
 final front = Image.asset('assets/images/fleur_de_lis.png');
 
 final cardsList = [
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/curvy_lines.png'),
     matchingId: 1,
+    hidden: true,
+    index: 1,
   ),
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/curvy_lines.png'),
     matchingId: 1,
+    hidden: true,
+    index: 2,
   ),
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/glass_house.png'),
     matchingId: 2,
+    hidden: true,
+    index: 3,
   ),
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/glass_house.png'),
     matchingId: 2,
+    hidden: true,
+    index: 4,
   ),
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/rooster.png'),
     matchingId: 3,
+    hidden: true,
+    index: 5,
   ),
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/rooster.png'),
     matchingId: 3,
+    hidden: true,
+    index: 6,
   ),
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/water.png'),
     matchingId: 4,
+    hidden: true,
+    index: 7,
   ),
-  MatchCard(
+  CardGridItem(
     front: front,
     back: Image.asset('assets/images/water.png'),
     matchingId: 4,
+    hidden: true,
+    index: 8,
   ),
 ];
