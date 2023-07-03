ed877500736f1c32304ba7f9eb361d1bd54bee50
Gabriele Peck <peck.gabriele@gmail.com>
2023-07-03 14:53:07 -0700

    turned into a StateNotifier, added a flipCard function

--- lib/providers/cards_provider.dart
+++ lib/providers/cards_provider.dart
@@ -1,4 +1,16 @@
 import 'package:card_match/data/cards_list.dart';
+import 'package:card_match/widgets/card_grid_item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final cardsProvider = Provider((ref) => cardsList);
+// final cardsProvider = Provider((ref) => cardsList);
+
+class CardsNotifier extends StateNotifier<List<CardGridItem>> {
+  CardsNotifier() : super(cardsList);
+
+  void flipCard(CardGridItem card, bool hidden) {
+    card.hidden = hidden;
+  }
+}
+
+final cardsProvider = StateNotifierProvider<CardsNotifier, List<CardGridItem>>(
+    (ref) => CardsNotifier());
